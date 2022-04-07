18a8e0f3effdb359bac65095a3330e5998e21232
Nwoye Samuel Chukwuebuka <nwoyechukwuebuka98@gmail.com>
2022-04-07 16:19:44 -0700

    quick refix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ long long int c;
 char d;
 float f;
 
-printf("Size of a char: %]u byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of an int: %]u byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of a long int: %]u byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long long int: %]u byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a float: %]u byte(s)\n", (unsigned long)sizeof(f));
-return (O);
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+return (0);
 }
