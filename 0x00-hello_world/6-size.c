dc37be5840d84baae866ce37c348180f5d3831c5
Nwoye Samuel Chukwuebuka <nwoyechukwuebuka98@gmail.com>
2022-04-07 14:03:30 -0700

    My first c programming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always O (Success)
+ */
+int main(void)
+{
+int a;
+long int b;
+long long int c;
+char d;
+float f;
+
+printf("Size of a char: %]u byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of an int: %]u byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a long int: %]u byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long long int: %]u byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a float: %]u byte(s)\n", (unsigned long)sizeof(f));
+return (O);
+}
