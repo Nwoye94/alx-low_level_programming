f0462dad3e02b0b1447415db8a48009f42c6fc80
Nwoye Samuel Chukwuebuka <nwoyechukwuebuka98@gmail.com>
2022-04-13 14:53:47 -0700

    repairs

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -15,7 +15,7 @@ unsigned long half1, half2;
 for (count = 0; count < 92; count++)
 {
 sum = fib1 + fib2;
-printf("%|u, ", sum);
+printf("%lu, ", sum);
 
 fib1 = fib2;
 fib2 = sum;
@@ -36,7 +36,7 @@ half1 += 1;
 half2 %= 10000000000;
 }
 
-printf("%|u%|u", half, half2);
+printf("%lu%lu", half1, half2);
 if (count != 98)
 printf(", ");
 
