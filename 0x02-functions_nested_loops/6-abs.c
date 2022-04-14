ebb1554ff9943bd0d39f42c2716ffa516ce3cd6d
Nwoye Samuel Chukwuebuka <nwoyechukwuebuka98@gmail.com>
2022-04-13 20:57:19 -0700

    checking

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _abi - Computes the absolute value of an integer.
+ * _abs - Computes the absolute value of an integer.
  * @n: The interger to be computed.
  *
  * Return: The absolute value of the integer.
  */
 int _abi(int n)
 {
-if (n >= 0)
-return (n);
+if (n < 0)
+return (n * -1);
 else
-return = _abi(-n);
+return (n);
 }
