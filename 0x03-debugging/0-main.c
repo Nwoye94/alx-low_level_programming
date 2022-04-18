f0ed109e47b304048b8034bac08de791c886ca14
Nwoye Samuel Chukwuebuka <nwoyechukwuebuka98@gmail.com>
2022-04-18 12:58:15 -0700

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if interger is negative or positive
+ * Return: zero
+ */
+
+int main(void)
+{
+int i;
+
+i = 0;
+positive_or_negative(i);
+
+return (0);
+}
