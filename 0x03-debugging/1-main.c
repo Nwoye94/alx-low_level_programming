9995aa4e9aa3417e052ca3e0e42b5de5ae843bd2
Nwoye Samuel Chukwuebuka <nwoyechukwuebuka98@gmail.com>
2022-04-18 23:31:22 -0700

    fixed

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - causes an infinite loop
  * Return: 0
@@ -16,6 +15,7 @@ i = 0;
 /**
  * while (i < 10)
  * {
+ * Infinite loop - No increase of variable
  * putchar(i);
  * }
  */
