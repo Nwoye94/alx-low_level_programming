8801d5d3a1c8c813d8d04942af2b4d5f320098a0
Nwoye Samuel Chukwuebuka <nwoyechukwuebuka98@gmail.com>
2022-05-06 06:32:53 -0700

    working

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/** isupper - print digits in upper case
+ *
+ * Return: always void
+ */
+
+int _isupper(int c)
+{
+	c = 0;
+	return c;
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @n: The interger to be computed.
+ *
+ * Return: The absolute value of the integer.
+ */
+int _abi(int n)
+{
+if (n < 0)
+return (n * -1);
+else
+return (n);
+}
